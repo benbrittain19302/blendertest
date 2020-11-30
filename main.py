ebde5a8248ccc75203c2ed4651d5026e2c9d631b
Your Name <you@example.com>
2020-11-30 17:34:47 +0000

    second workshop, importing model etc

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import bpy
 import math
 import random
+import os
 
-def create_cube(name, position, scale, rotateB):
+bpy.ops.object.select_all(action='DESELECT')
+for obj in bpy.data.objects:
+    if obj.type == 'MESH':
+        obj.select_set(True)
+bpy.ops.object.delete()
+
+def create_cube(name, position, scale):
     bpy.ops.mesh.primitive_cube_add()
     cube = bpy.context.selected_objects[0]
     cube.name = name
     cube.location = position
     cube.scale = scale
-    if rotateB:
-        cube.rotation_euler = [math.radians(random.randint(10,30)),math.radians(random.randint(10,30)),math.radians(random.randint(10,30))]
     
-create_cube("floor",[0,0,0],[10,10,.1], False)
+"""
+create_cube("floor",[0,0,0],[10,10,.1])
 
 for x in range(10):
     if x % 2 == 0:    
-        create_cube("stair",[0,x,1+(x/2)],[3,1,1+(x/2)], True)
+        create_cube("stair",[0,x,1+(x/2)],[3,1,1+(x/2)])
+           
+        
+for env in os.environ:
+    print(env)
+"""
+path = 'export/myfile.obj'
+bpy.ops.import_scene.obj(filepath=path)
+obj = bpy.context.selected_objects[0]
+
 
  
  
